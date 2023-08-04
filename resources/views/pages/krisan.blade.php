c4380ae6a28620b00d2d881f368a39d74ba85f81
ahmadnial <ahmadhky@gmail.com>
2023-08-04 15:28:47 +0700

    Rubah metode dan item order

--- resources/views/pages/krisan.blade.php
+++ resources/views/pages/krisan.blade.php
@@ -17,9 +17,9 @@
                         <th>Nama</th>
                         <th>Pasien ID</th> 
                         <th>DPJD</th> 
-                        <th>Input Diet</th>
-                        <th>Action</th>
+                        {{-- <th>Input Diet</th> --}}
                         <th>Diet</th>
+                        <th>Action</th>
                     </tr>
                 </thead> 
                 <?php $no='1'; ?>
@@ -32,30 +32,145 @@
                         <td>{{$item['PasienName']}}</td>
                         <td>{{$item['PasienID']}}</td>
                         <td>{{$item['DokterName']}}</td>
-                        <form method="POST" action="{{ url('/insert-diet') }}"> 
-                            @csrf
-                        <td>
-                            <input type="hidden" name="bed" value="{{$item['BedName']}}">
-                            <input type="hidden" name="nama" value="{{$item['PasienName']}}"> 
-                            <input type="hidden" name="pasienID" value="{{$item['PasienID']}}">
-                            <input type="hidden" name="DPJP" value="{{$item['DokterName']}}">
-                            <input type="text" class="form-control" name="diet">
-                        </td>
-                        <td>
+                        <td class="warning">
                             @foreach ($getval as $ze)
                             @if ($item['PasienID']==$ze['pasienID']) 
                             {{ $ze->diet }} <i class="fa fa-check"></i>
-                            @else
+                            @else 
                             {{ '' }} 
-                            @endif
+                            @endif 
                             @endforeach
                         </td>
-                        <td>
-                            <button type="submit" class="btn btn-sm btn-success" name="click" id="click" onclick="zimbabwe(this)"><i class="fa fa-save"></i></button>
+                        <td class="">
+                            <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#modalTambah{{$item['PasienID']}}"><i class="fa fa-plus"> Add</i></button>
+                            <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modalViewEdit{{$item['PasienID']}}"><i class="fa fa-folder-open"> View/Edit</i></button>
+                            {{-- <button type="" class="btn btn-sm btn-warning" name="edit" id="edit"><i class="fa fa-edit"></i></button> --}}
                         </td>  
-                        @endif
-                    </form>
-                </tr>
+                    @endif
+                </tr> 
+                 {{-- modal --}}
+
+                    <div class="modal fade" id="modalTambah{{$item['PasienID']}}" tabindex="-1" aria-labelledby="modalTambah" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header bg-primary">
+                                    <h5 class="modal-title">Input Diet</h5>
+                                        {{-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button> --}}
+                                        </div>
+                                            <div class="modal-body">
+                                            <!--FORM TAMBAH BARANG-->
+                                            <form action="{{ url('/addDiet') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" class="form-control" id="bed" name="bed" value="{{$item['BedName']}}">
+                                                <input type="hidden" class="form-control" id="nama" name="nama" value="{{$item['PasienName']}}">
+                                                <input type="hidden" class="form-control" id="pasienID" name="pasienID" value="{{$item['PasienID']}}">
+                                                <input type="hidden" class="form-control" id="DPJP" name="DPJP" value="{{$item['DokterName']}}"> 
+
+                                                <div class="hdr">
+                                                    Diet Untuk Pasien a.n : <b>{{$item['PasienName']}}</b> , Ruang : <b>{{$item['BedName']}}</b>
+                                                </div>
+                                                <br>
+                                                <div class="form-group">
+                                                    <label for="">Diet Pagi</label>
+                                                    <input type="text" class="form-control" id="diet_pagi" name="diet_pagi" placeholder="Masukan Jenis Diet..">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" id="diet_pagi_konsistensi" name="diet_pagi_konsistensi" placeholder="Masukan Konsistensi..">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="">Diet Siang</label>
+                                                    <input type="text" class="form-control" id="diet_siang" name="diet_siang" placeholder="Masukan Jenis Diet..">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" id="diet_siang_konsistensi" name="diet_siang_konsistensi" placeholder="Masukan Konsistensi..">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="">Diet Sore</label>
+                                                    <input type="text" class="form-control" id="diet_sore" name="diet_sore" placeholder="Masukan Jenis Diet..">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" id="diet_sore_konsistensi" name="diet_sore_konsistensi" placeholder="Masukan Konsistensi..">
+                                                </div>
+
+                                            <div class="float-right">
+                                                <button type="submit" class="btn btn-success float-right"><i class="fa fa-save"> Simpan</i></button>
+                                            </div>
+                                            </form>
+                                            <!--END FORM TAMBAH BARANG-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    {{-- END MODAL Add --}}
+
+                    {{-- modal view-Edit --}}
+
+                    <div class="modal fade" id="modalViewEdit{{$item['PasienID']}}" tabindex="-1" aria-labelledby="modalTambah" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header bg-warning">
+                                    <h5 class="modal-title">View Diet</h5>
+                                        <a class="close" data-dismiss="modal" aria-label="Close">
+                                        {{-- <span aria-hidden="true">&times;</span> --}} <i class="fa fa-close"></i>
+                                        </a>
+                                        </div>
+                                            <div class="modal-body">
+                                            <!--FORM VIEW EDIT-->
+                                            <form action="{{ url('/viewEdit') }}/{{$item['PasienID']}}" method="put">
+                                                @csrf
+                                                <div class="hdr">
+                                                    Diet Untuk Pasien a.n : <b>{{$item['PasienName']}}</b> , Ruang : <b>{{$item['BedName']}}</b>
+                                                </div>
+                                                <br>
+                                                @foreach ($getval as $ze)
+                                                @if ($item['PasienName']==$ze['nama'])
+                                                    
+                                                <div class="form-group">
+                                                    <label for="">Diet Pagi</label>
+                                                    <input type="text" class="form-control" id="diet_pagi" name="diet_pagi" placeholder="Masukan Jenis Diet.." value="{{$ze['diet_pagi']}}">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" id="diet_pagi_konsistensi" name="diet_pagi_konsistensi" placeholder="Masukan Konsistensi.." value="{{$ze['diet_pagi_konsistensi']}}">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="">Diet Siang</label>
+                                                    <input type="text" class="form-control" id="diet_siang" name="diet_siang" placeholder="Masukan Jenis Diet.." value="{{$ze['diet_siang']}}">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" id="diet_siang_konsistensi" name="diet_siang_konsistensi" placeholder="Masukan Konsistensi.." value="{{$ze['diet_siang_konsistensi']}}">
+                                                </div>
+                                                
+                                                <div class="form-group">
+                                                    <label for="">Diet Sore</label>
+                                                    <input type="text" class="form-control" id="diet_sore" name="diet_sore" placeholder="Masukan Jenis Diet.." value="{{$ze['diet_sore']}}">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" id="diet_sore_konsistensi" name="diet_sore_konsistensi" placeholder="Masukan Konsistensi.." value="{{$ze['diet_sore_konsistensi']}}">
+                                                </div>
+                                                
+                                                <div class="button float-right">
+                                                    <button type="submit" class="btn btn-success float-right"><i class="fa fa-pencil-square-o"> &nbsp; Update</i></button>
+                                            </div>
+                                        </form>
+                                        <!--END FORM TAMBAH BARANG-->
+                                        {{-- @else
+                                            <div class="bg-danger">
+                                                {{'kosong! Entahlah,Sepertinya Jenengan Belum Input Data Diet'}}
+                                            </div>                                                --}}
+                                        @endif
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    {{-- END MODAL --}}
                 @endforeach 
             </tbody>
             </table>

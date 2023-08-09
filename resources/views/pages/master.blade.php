013eeac56acd31d2dd7f0e1c1635f16d9653cc35
ahmadnial <ahmadhky@gmail,com>
2023-08-09 09:46:10 +0700

    asset

--- resources/views/pages/master.blade.php
+++ resources/views/pages/master.blade.php
@@ -7,29 +7,29 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href=" {{ asset('/src/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/bootstrap/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href=" {{ asset('/src/dist/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/dist/css/AdminLTE.min.css') }}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href=" {{ asset('/src/dist/css/skins/_all-skins.min.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/dist/css/skins/_all-skins.min.css') }}">
   <!-- iCheck -->
-  <link rel="stylesheet" href=" {{ asset('/src/plugins/iCheck/flat/blue.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/plugins/iCheck/flat/blue.css') }}">
   <!-- Morris chart -->
-  <link rel="stylesheet" href=" {{ asset('/src/plugins/morris/morris.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/plugins/morris/morris.css') }}">
   <!-- jvectormap -->
-  <link rel="stylesheet" href=" {{ asset('/src/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
   <!-- Date Picker -->
-  <link rel="stylesheet" href=" {{ asset('/src/plugins/datepicker/datepicker3.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/plugins/datepicker/datepicker3.css') }}">
   <!-- Daterange picker -->
   <link rel="stylesheet" href=" {{ asset('/src/plugins/daterangepicker/daterangepicker-bs3.css') }}">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href=" {{ asset('/src/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+  <link rel="stylesheet" href=" {{ asset('src/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
 
 
 </head>
